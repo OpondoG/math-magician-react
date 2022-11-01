43de3e5fc5f2fbeae3905ec193cac4a271c9004a
OpondoG <okonjogilbert2@gmail.com>
2022-11-01 18:53:01 +0300

    Add 3 pages to my website

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './Components/Calculator';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Quote from './Components/Quote';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const App = () => (
+  <Router>
+    <Header />
 
-  render() {
-    return <Calculator />;
-  }
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Calculator" element={<Calculator />} />
+      <Route path="/Quote" element={<Quote />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
